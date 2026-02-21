b2c1057758e77f07c0a63fde37eff7638f3f229a
xiaoli <boy1869@qq.com>
2026-02-21 17:13:38 +0800

    一些调整

--- src/app/[lng]/about/data/blogs.ts
+++ src/app/[lng]/about/data/blogs.ts
@@ -9,6 +9,14 @@ export interface Blog {
 
 export const blogsData: Blog[] = [
   {
+    image: "/images/bk2.webp",
+    titleKey: "blog.fund_valuation.title",
+    tagKey: "blog.article.development",
+    readTimeKey: "blog.fund_valuation.read_time",
+    dateKey: "blog.fund_valuation.date",
+    link: "https://juejin.cn/post/7602634187284054026",
+  },
+  {
     image: "/images/next_i18n.png",
     titleKey: "blog.article.title",
     tagKey: "blog.article.development",
